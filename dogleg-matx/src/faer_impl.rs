22ccdd50c7597e872a1c9da55d9c52c893b61d60
geo-ant <54497890+geo-ant@users.noreply.github.com>
2025-10-29 20:02:38 +0100

    much more generic faer impls

--- dogleg-matx/src/faer_impl.rs
+++ dogleg-matx/src/faer_impl.rs
@@ -1,8 +1,58 @@
+use crate::{Addx, Colx, Dotx, Matx, Ownedx, Scalex, TrMatVecMulx, TransformedVecNorm};
+use faer::col::AsColMut;
+use faer::mat::AsMatRef;
+use faer::{col::AsColRef, traits::RealField, Col, ColMut, ColRef, Mat, Scale};
+use faer::{Accum, MatMut, MatRef, Shape};
+use num_traits::ConstOne;
 use std::ops::AddAssign;
 
-use crate::{Addx, Colx, Dotx, Matx, Ownedx, Scalex};
-use faer::Shape;
-use faer::{col::AsColRef, traits::RealField, Col, ColMut, ColRef, Mat, Scale};
+/// marker trait to get around some conflicting impl trouble with nalgebra
+/// implementations.
+pub trait FaerType {}
+impl<T, R, C> FaerType for Mat<T, R, C>
+where
+    T: Clone,
+    R: faer::Shape,
+    C: faer::Shape,
+{
+}
+
+impl<T, R, C> FaerType for MatRef<'_, T, R, C>
+where
+    T: Clone,
+    R: faer::Shape,
+    C: faer::Shape,
+{
+}
+
+impl<T, R, C> FaerType for MatMut<'_, T, R, C>
+where
+    T: Clone,
+    R: faer::Shape,
+    C: faer::Shape,
+{
+}
+
+impl<T, R> FaerType for Col<T, R>
+where
+    T: Clone,
+    R: faer::Shape,
+{
+}
+
+impl<T, R> FaerType for ColRef<'_, T, R>
+where
+    T: Clone,
+    R: faer::Shape,
+{
+}
+
+impl<T, R> FaerType for ColMut<'_, T, R>
+where
+    T: Clone,
+    R: faer::Shape,
+{
+}
 
 impl<T, R, C> Matx<T> for Mat<T, R, C>
 where
@@ -21,12 +71,47 @@ where
     }
 }
 
+impl<T, R, C> Matx<T> for MatRef<'_, T, R, C>
+where
+    T: Clone + RealField,
+    R: faer::Shape,
+    C: faer::Shape,
+{
+    type Owned = Mat<T, R, C>;
+
+    fn into_owned(self) -> Self::Owned {
+        self.to_owned()
+    }
+
+    fn clone_owned(&self) -> Self::Owned {
+        self.to_owned()
+    }
+}
+
+impl<T, R, C> Matx<T> for MatMut<'_, T, R, C>
+where
+    T: Clone + RealField,
+    R: faer::Shape,
+    C: faer::Shape,
+{
+    type Owned = Mat<T, R, C>;
+
+    fn into_owned(self) -> Self::Owned {
+        self.to_owned()
+    }
+
+    fn clone_owned(&self) -> Self::Owned {
+        self.to_owned()
+    }
+}
+
 impl<T, R, C> Ownedx for Mat<T, R, C>
 where
     R: Shape,
     C: Shape,
 {
 }
+
 impl<T, R> Ownedx for Col<T, R> where R: Shape {}
 
 impl<T, R> Colx<T> for Col<T, R>
@@ -110,39 +195,41 @@ where
     }
 }
 
-impl<T, V, R> Addx<T, V> for Col<T, R>
+impl<T, V, R, ColType> Addx<T, V> for ColType
 where
+    ColType: FaerType + AsColMut<T = T, Rows = R>,
     T: RealField + Copy,
     T: AddAssign,
     V: AsColRef<T = T, Rows = R> + Colx<T>,
     R: Shape,
 {
     fn scaled_add(mut self, factor: T, y: &V) -> Option<Self> {
+        let this = self.as_col_mut();
         let yref = y.as_col_ref();
-        if self.nrows() != yref.nrows() {
+        if this.nrows() != yref.nrows() {
             return None;
         }
         //@todo(geo-ant): PERF: is this the most efficient way of implementing
         // this axpy variant?
         #[allow(unused_mut)]
-        faer::zip!(&mut self, yref).for_each(|faer::unzip!(mut this, rhs)| *this += factor * *rhs);
+        faer::zip!(this, yref).for_each(|faer::unzip!(mut this, rhs)| *this += factor * *rhs);
         Some(self)
     }
 }
 
-impl<T, V, R> Dotx<T, V> for Col<T, R>
+impl<T, V, R, V1> Dotx<T, V> for V1
 where
+    V1: FaerType + AsColRef<T = T, Rows = R> + Colx<T>,
     T: RealField,
     R: Shape,
     V: Colx<T> + AsColRef<T = T, Rows = R>,
 {
     fn dot(&self, v: &V) -> Option<T> {
         let v = v.as_col_ref();
-        if self.nrows() != v.nrows() {
+        let this = self.as_col_ref();
+        if this.nrows() != v.nrows() {
             return None;
         }
-
-        let this = self.as_col_ref();
 
         Some(faer::linalg::matmul::dot::inner_prod(
             this.transpose(),
@@ -150,5 +237,40 @@ where
             v,
             faer::Conj::No,
         ))
+    }
+}
+
+impl<T, R, C, V, M> TrMatVecMulx<T, V> for M
+where
+    M: AsMatRef<T = T, Rows = R, Cols = C> + FaerType,
+    T: RealField + ConstOne,
+    R: Shape,
+    C: Shape,
+    V: Colx<T> + AsColRef<T = T, Rows = R>,
+{
+    type Output = Col<T, C>;
+
+    fn tr_mulv(&self, v: &V) -> Option<Self::Output> {
+        let m = self.as_mat_ref();
+        let v = v.as_col_ref();
+        let (transposed_cols, transposed_rows) = (m.nrows(), m.ncols());
+
+        let v_rows = v.nrows();
+        if v_rows != transposed_cols {
+            return None;
+        }
+
+        //@todo(geo-ant) PERFORMANCE: is there a way to have uninitialized
+        // values here?
+        let mut dest = Col::<T, C>::zeros(transposed_rows);
+        faer::linalg::matmul::matmul(
+            &mut dest,
+            Accum::Replace,
+            m.transpose(),
+            v,
+            T::ONE,
+            faer::get_global_parallelism(),
+        );
+        Some(dest)
     }
 }

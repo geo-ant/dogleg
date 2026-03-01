1761e9990abcf6f2c9e7c72469004529a012f5c5
geo-ant <54497890+geo-ant@users.noreply.github.com>
2026-03-01 22:34:53 +0100

    recursive path handling

--- templated-tests/src/template.rs
+++ templated-tests/src/template.rs
@@ -15,7 +15,8 @@ impl FunctionTemplateInstantiation {
 }
 
 impl syn::fold::Fold for FunctionTemplateInstantiation {
-    fn fold_path(&mut self, mut path: syn::Path) -> syn::Path {
+    fn fold_path(&mut self, path: syn::Path) -> syn::Path {
+        let mut path = syn::fold::fold_path(self, path);
         if let Some(segment) = path.segments.first_mut() {
             if segment.ident == self.template_ident {
                 segment.ident = self.concrete_type.clone();
